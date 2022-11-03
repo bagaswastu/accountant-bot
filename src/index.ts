de16d3205ddc2dc309aa4026151f3c0dbf13f299
Bagas Wastu <67826350+bagaswastu@users.noreply.github.com>
2022-11-03 14:00:51 +0700

    chore: configure grammy

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello world');
+import * as dotenv from 'dotenv';
+import { Bot } from 'grammy';
+
+dotenv.config();
+const bot = new Bot(process.env.BOT_TOKEN!);
+
+bot.command('info', (ctx) => {
+  ctx.reply('Hello world!');
+});
+
+bot.start();

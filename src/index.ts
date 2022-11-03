1bc59fc830bfa27778e3ddcaa11e9848f8eb9e78
Bagas Wastu <67826350+bagaswastu@users.noreply.github.com>
2022-11-03 22:22:20 +0700

    refactor: add auth and loading middleware

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,34 @@
 import * as dotenv from 'dotenv';
-import { Bot } from 'grammy';
+import { Bot, Context } from 'grammy';
 import { PrismaClient } from '@prisma/client';
+import { hydrate, hydrateApi, HydrateFlavor } from '@grammyjs/hydrate';
 
 dotenv.config();
-const bot = new Bot(process.env.BOT_TOKEN!);
+const bot = new Bot<HydrateFlavor<Context>>(process.env.BOT_TOKEN!);
 const prisma = new PrismaClient();
 
-bot.command('info', (ctx) => {
-  ctx.reply('Hello world!');
+bot.use(hydrate());
+
+// loading middleware
+bot.use(async (ctx, next) => {
+  const loadingMessage = await ctx.reply('⏳ Please wait...', {
+    disable_notification: true,
+  });
+  await next();
+  await loadingMessage.delete();
+});
+
+// authentication middleware
+bot.use(async (ctx, next) => {
+  const userIds = process.env.LIST_USER_ID?.split(' ');
+
+  if (!userIds?.includes(ctx.from?.id.toString()!)) {
+    ctx.reply('Unauthorized');
+    return;
+  }
+
+  await next();
+});
 });
 
 bot.start();

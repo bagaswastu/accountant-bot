dbe3e74e7ef4815f587f6f84a7eda174b52d5a26
Bagas Wastu <67826350+bagaswastu@users.noreply.github.com>
2022-11-04 22:05:36 +0700

    feat: add condition when no category was found

--- src/index.ts
+++ src/index.ts
@@ -227,6 +227,11 @@ bot.on(':text').hears(/(.+) ?= ?(.+)/, async (ctx) => {
 bot.command('list_category', async (ctx) => {
   const categories = await prisma.category.findMany();
 
+  if (categories.length === 0) {
+    await ctx.reply('No category found');
+    return;
+  }
+
   const categoriesStr = categories
     .filter((c) => c.name !== 'uncategorized')
     .map((category) => `➣ ${category.name}`)

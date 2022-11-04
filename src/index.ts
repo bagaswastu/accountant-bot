7769e2a53004141bcd17bb7febf688137face095
Bagas Wastu <67826350+bagaswastu@users.noreply.github.com>
2022-11-04 22:19:12 +0700

    feat: add create category command

--- src/index.ts
+++ src/index.ts
@@ -246,4 +246,38 @@ ${categoriesStr}
   );
 });
 
+/**
+ * Create category command.
+ *
+ * Matches:
+ *  /create_category {category}
+ * Example:
+ *  /create_category food
+ */
+bot.command('create_category', async (ctx) => {
+  const categoryName = ctx.match;
+
+  if (!categoryName) {
+    await ctx.reply('Please provide category name');
+    return;
+  }
+
+  try {
+    const category = await prisma.category.create({
+      data: {
+        id: nanoid(),
+        name: categoryName,
+      },
+    });
+    await ctx.reply(`Category *${category.name}* created`, {
+      parse_mode: 'MarkdownV2',
+    });
+  } catch (e: any) {
+    // if unique constraint error
+    if (e.code === 'P2002') {
+      throw Error(`Category ${categoryName} already exists`);
+    }
+  }
+});
+
 bot.start();

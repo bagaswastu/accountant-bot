fe3dcdf897b3dc2fc693c0236c52ace512f2f9df
Bagas Wastu <67826350+bagaswastu@users.noreply.github.com>
2022-11-10 11:58:06 +0700

    refactor: more emoji ❤️

--- src/bot.ts
+++ src/bot.ts
@@ -1,16 +1,16 @@
 import { conversations } from '@grammyjs/conversations';
 import { hydrate } from '@grammyjs/hydrate';
 import { Bot, session } from 'grammy';
+import { lists as listCategory } from './category/categories';
+import { lists as detailCategory } from './category/category_[id]';
 import { lists as createCategory } from './category/create_category';
 import { lists as deleteCategory } from './category/delete_category_[id]';
-import { lists as detailCategory } from './category/category_[id]';
-import { lists as listCategory } from './category/categories';
 import { lists as updateCategory } from './category/update_category_[id]';
 import { lists as categorize } from './expense/categorize';
 import { lists as createExpense } from './expense/create_expense';
 import { lists as deleteExpense } from './expense/delete_expense_[id]';
-import { lists as detailExpense } from './expense/expense_[id]';
 import { lists as listExpense } from './expense/expenses';
+import { lists as detailExpense } from './expense/expense_[id]';
 import { CustomContext } from './lib/types';
 import { lists as loading } from './middleware/loading';
 import { lists as auth } from './misc/auth';
@@ -35,10 +35,10 @@ bot.use(loading);
 bot.command('cancel', async (ctx) => {
   const conversations = await ctx.conversation.active();
   if (Object.keys(conversations).length === 0)
-    return ctx.reply(`You don't have any active operation`);
+    return ctx.reply(`❌ You don't have any active operation`);
 
   await ctx.conversation.exit();
-  await ctx.reply('Operation cancelled.');
+  await ctx.reply('✅ Operation cancelled');
 });
 
 // expense
@@ -59,3 +59,8 @@ bot.use(listCategory);
 bot.use(report);
 bot.use(auth);
 bot.use(info);
+
+// unknown command
+bot.on('message', async (ctx) => {
+  await ctx.reply('❌ Unknown command');
+});

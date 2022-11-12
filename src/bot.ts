d92f64c84aa64294e8e755d4009e18fff8227aa0
Bagas Wastu <67826350+bagaswastu@users.noreply.github.com>
2022-11-12 22:30:40 +0700

    refactor: clean state

--- src/bot.ts
+++ src/bot.ts
@@ -3,19 +3,6 @@ import { hydrate } from '@grammyjs/hydrate';
 import { Bot, session } from 'grammy';
 import { CustomContext } from './lib/types';
 import { lists as auth } from './middleware/auth';
-import { composer as start } from './commands/start';
-import { composer as createExpense } from './global/create-expense';
-import { composer as categorize } from './global/categorize';
-import { composer as addCategory } from './commands/addcategory';
-import { composer as detailExpense } from './commands/expense_[id]';
-import { composer as deleteExpense } from './commands/delete_expense_[id]';
-import { composer as detailCategory } from './commands/category_[id]';
-import { composer as deleteCategory } from './commands/delete_category_[id]';
-import { composer as updateCategory } from './commands/update_category_[id]';
-import { composer as categories } from './commands/categories';
-import { composer as expenses } from './commands/expenses';
-import { composer as report } from './commands/report';
-import { composer as timezone } from './commands/timezone';
 
 if (process.env.BOT_TOKEN == null) throw new Error('BOT_TOKEN is missing.');
 export const bot = new Bot<CustomContext>(process.env.BOT_TOKEN!);
@@ -31,19 +18,6 @@ bot.use(conversations());
 bot.use(hydrate());
 
 bot.use(auth);
-bot.use(start);
-bot.use(createExpense);
-bot.use(categorize);
-bot.use(addCategory);
-bot.use(detailExpense);
-bot.use(deleteExpense);
-bot.use(detailCategory);
-bot.use(deleteCategory);
-bot.use(updateCategory);
-bot.use(expenses);
-bot.use(categories);
-bot.use(report);
-bot.use(timezone);
 
 bot.catch((err) => {
   err.ctx.reply(err.message);

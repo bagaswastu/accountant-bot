687493f1befb75bd0db554c955133cf243523770
Bagas Wastu <67826350+bagaswastu@users.noreply.github.com>
2022-11-13 18:42:22 +0700

    feat: create transaction command

--- src/bot.ts
+++ src/bot.ts
@@ -1,21 +1,20 @@
 import { conversations } from '@grammyjs/conversations';
 import { hydrate } from '@grammyjs/hydrate';
 import { Bot, session } from 'grammy';
-import { CustomContext } from './lib/types';
+import { CustomContext, SessionData } from './lib/types';
 import { lists as auth } from './middleware/auth';
+import { composer as createTransaction } from './commands/create-transaction';
 
 if (process.env.BOT_TOKEN == null) throw new Error('BOT_TOKEN is missing.');
 export const bot = new Bot<CustomContext>(process.env.BOT_TOKEN!);
 
-bot.use(
-  session({
-    initial() {
-      return {};
-    },
-  })
-);
+function initial(): SessionData {
+  return { pendingTransaction: null };
+}
+bot.use(session({ initial }));
 bot.use(conversations());
 bot.use(hydrate());
+bot.use(createTransaction);
 
 bot.use(auth);
 
@@ -24,16 +23,25 @@ bot.catch((err) => {
   console.error(err);
 });
 
-// Always exit any conversation when the user sends /cancel
-bot.command('cancel', async (ctx) => {
-  const conversations = await ctx.conversation.active();
-  if (Object.keys(conversations).length === 0) return ctx.reply('Umm, what?');
+bot.on(':text', (context) => {
+  if (context.session.pendingTransaction) {
+    context.reply(
+      `You have a pending transaction\n\nTransaction detail: ${context.session.pendingTransaction.description}.`
+    );
+  }
+});
 
-  await ctx.conversation.exit();
-  await ctx.reply('Okay, cancelled that.');
+// Always exit any conversation when the user sends /cancel
+bot.command('cancel', async (context) => {
+  const conversations = await context.conversation.active();
+  if (Object.keys(conversations).length === 0)
+    return context.reply('Umm, what?');
+
+  await context.conversation.exit();
+  await context.reply('Okay, cancelled that.');
 });
 
 // unknown command
-bot.on('message', async (ctx) => {
-  await ctx.reply(`I don't get what you mean, mind to check /start?`);
+bot.on('message', async (context) => {
+  await context.reply(`I don't get what you mean, mind to check /start?`);
 });
